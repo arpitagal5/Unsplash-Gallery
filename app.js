b78588dc3e41677f0bb3e91e9d32a825221c66ee
arpitagal5 <arpitagal5@users.noreply.github.com>
2021-06-03 17:45:38 +0530

    Inital Commit

--- app.js
+++ app.js
@@ -4,27 +4,57 @@ const imageContainer = document.querySelector('.container');
 const nextPage = document.querySelector('.next');
 const auth = 'HcJtaOVPE6d_1ynLmDQFK_jUmcJnTTP21Lx68hczY78';
 
-let query = '' ;
+let query = 'cake' ;
+let search = false ;
 let pagenm = 1 ;
+getImages();
 
 searchButton.addEventListener('click' , e => {
     e.preventDefault();
+    if(inputText.value.length >= 0){
+        query = inputText.value;
+        deleteChildren();
+    }
+    
     getImages();
 })
 
+function deleteChildren(){
+    let lastChild = imageContainer.lastElementChild ;
+    while(lastChild){
+        imageContainer.removeChild(lastChild);
+        lastChild = imageContainer.lastElementChild;
+    }
+}
+
 async function getImages(){
-    query = inputText.value ;
-    let response = await fetch(`https://api.unsplash.com/search/photos?client_id=${auth}&query=${query}&per_page=10&orientation=portrait&page=${pagenm}`);
+    try{
+        let response = await fetch(`https://api.unsplash.com/search/photos?client_id=${auth}&query=${query}&per_page=12&orientation=portrait&page=${pagenm}`);
     let data = await response.json();
     console.log(data , typeof data);
     data.results.forEach(element => {
-        const img = document.createElement('img');
-        img.src = element.urls.full ;
-        imageContainer.appendChild(img);
+        const image = `
+        <div class="conatiner_img">
+        <a href="${element.urls.full}">
+        <img src="${element.urls.full}" alt="${element.alt_description}" /></a>
+        <div class="hover_content">
+        <p>${element.user.first_name}</p>
+        <p>DOWNLOAD</p>
+        </div>
+        </div>`;
+        imageContainer.innerHTML += image;
         
-    });
-   
+      });
+    }catch{
+        console.log("Server Error");
+
+    }   
 }
-// fetch(`https://api.unsplash.com/photos?client_id=${auth}&query=fire&per_page=30&orientation=portrait&page=1`)
-// .then(response => response.json())
-// .then(data => console.log(data));
+nextPage.addEventListener('click' , () => {
+    if(!search){
+        pagenm++ ;
+        getImages();
+    }
+
+})
+
